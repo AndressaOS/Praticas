2abbde459bc83e5f35503b250aad485af9bb79f8
AndressaOS <andressaoliveirasouz@gmail.com>
2021-03-29 22:58:23 -0300

    Update exercicio6.cpp

--- Lista2/exercicio6.cpp
+++ Lista2/exercicio6.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <vector>
 
+//Algoritmo baseado em https://www.geeksforgeeks.org/convex-hull-set-1-jarviss-algorithm-or-wrapping/
+
 using namespace std;
 
-// Algoritmo que dado um conjunto de pontos S calcula a sua casca convexa (convex_hull)
 
 struct Ponto{
 	int x, y;
 	};
-	
+
 vector<Ponto> pontosCasca;
+vector<Ponto> pontos;	
+
 
 	
 //verifica o lado da reta que está o ponto
@@ -30,17 +33,65 @@ if(total>c)
 		if(total ==c)
 			return 0;
 			else
-				return -1;
+				return 2;
 	}
 	
+}
+
+//verifica ponto mais a esquerda
+int verificaPontoMaisAEsquerda(){
+		int posicao = 0;
+    	for (int i = 1; i < pontos.size(); i++){
+    		if (pontos[i].x < pontos[posicao].x)
+           		posicao= i;
+		}
+		if(posicao!=0){
+			return posicao;
+	      
+		}else{
+			return 0;
+		}
+}
+
+void cascaConvexa(){
+	if(pontos.size()>3){
+		int posicao, l;
+		l= verificaPontoMaisAEsquerda();
+		posicao = l; 
+		int q;
+    do
+    {
+        pontosCasca.push_back(pontos[posicao]);
+        q = (posicao+1)%pontos.size();
+        for (int i = 0; i < pontos.size(); i++){
+            if (verificaPonto(pontos[posicao], pontos[i], pontos[q]) == 2)
+               q = i;
+        }
+        posicao = q;
+  
+    } while (posicao != l);  
+	}	
+	else{
+		if(pontos.size()==3){		
+			for(int i=0; i<3; i++){
+				pontosCasca.push_back(pontos[i]);
+			}
+		}
+				
+		else
+			printf("Quantidade de pontos insuficiente.\n");
+	}
 }
 
 
 
 int main(int argc, char** argv) {
 	
-	vector <Ponto> pontos;
+	
 	Ponto p1, p2, p3, p4, p5, p6;
+	
+	//Teste 1
+/*
 	p1.x=1;
 	p1.y=1;
 	pontos.push_back(p1);
@@ -56,101 +107,57 @@ int main(int argc, char** argv) {
 	p5.x=5;
 	p5.y=6;
 	pontos.push_back(p5);
+	p6.x=3;
+	p6.y=2;
+	pontos.push_back(p6);*/
 	
-	if(pontos.size()>3){
+	//Teste 2
 
-		//verifica o elemento mais a esquerda
-		int posicao = 0;
-    	for (int i = 1; i < pontos.size(); i++){
-    		if (pontos[i].x < pontos[posicao].x)
-           		posicao= i;
-		}
-		if(posicao!=0){
-			Ponto aux;
-			aux = pontos[0];
-			pontos[0]= pontos[posicao];
-			pontos[posicao]=aux;
-			pontosCasca.push_back(pontos[posicao]);
-	      
-		}else{
-			pontosCasca.push_back(pontos[0]);
-		}
+	p1.x=1;
+	p1.y=1;
+	pontos.push_back(p1);
+	p2.x=20;
+	p2.y=-5;
+	pontos.push_back(p2);
+	p3.x=5;
+	p3.y=1;
+	pontos.push_back(p3);
+	p4.x=20;
+	p4.y=12;
+	pontos.push_back(p4);
+	p5.x=-30;
+	p5.y=10;
+	pontos.push_back(p5);
+	p6.x=-30;
+	p6.y=-15;
+	pontos.push_back(p6);
 	
-		
-		  
+	//Teste 3
+	/*
+	p1.x=-1;
+	p1.y=-1;
+	pontos.push_back(p1);
+	p2.x=-1;
+	p2.y=5;
+	pontos.push_back(p2);
+	p3.x=5;
+	p3.y=1;
+	pontos.push_back(p3);
+	p4.x=2;
+	p4.y=4;
+	pontos.push_back(p4);
+	p5.x=5;
+	p5.y=6;
+	pontos.push_back(p5);
+	p6.x=3;
+	p6.y=2;
+	pontos.push_back(p6);*/
 	
-		int valorAnterior, valorAtual, p1;
-		for(int i=0; i<pontos.size(); i++){
-			
-					
-			for(int j=i+1; j<pontos.size(); j++){
-				printf("\n 1 ( %d , %d  ) 2 ( %d , %d  ) ", pontos[i].x, pontos[i].y, pontos[j].x, pontos[j].y  );
-				valorAnterior=-2;
-				
-			
-					for(int k=0; k<pontos.size(); k++){
-						
-						if(k!=j && k!=i){
-							
-							printf("\n 3 ( %d , %d  )", pontos[k].x, pontos[k].y );
-							valorAtual = verificaPonto(pontos[i], pontos[j], pontos[k]);
-							printf("\n i %d j %d k %d", i, j, k);
-							printf(" %d \n", valorAtual);
-								printf(" %d ", valorAnterior);
-				
-				   		if(valorAnterior==-2){
-							valorAnterior= verificaPonto(pontos[i], pontos[j], pontos[k]);
-						}else{
-						
-							if((valorAnterior!=valorAtual) && (valorAnterior!=0 && valorAtual!=0) && valorAnterior!=-2){
-								  valorAnterior=3;
-								  
-									printf("oi");
-								//	break;
-							
-							}				
-							else{
-									valorAnterior= valorAtual;
-								}
-						}
-					/*	if(verificaPonto(pontos[i], pontos[j], pontos[k])==0){		
-						printf("oi /n");						
-							pontosCasca.push_back(pontos[j]);
-							pontosCasca.push_back(pontos[k]);
-												
-						}*/
-						}
-					
-				
-				}	p1=j;
-				
-				printf("fim %d", valorAnterior);
-				if(valorAnterior!=3){
-						printf(" %d tt \n", valorAnterior);
-						pontosCasca.push_back(pontos[p1]);
-						
-									
-				}
-			
-			} 
-				
-		}
-		
-		for(int i=0; i<pontosCasca.size();i++){
-	
+	cascaConvexa();
+
+	for(int i=0; i<pontosCasca.size(); i++){
 		printf("\n Ponto ( %d , %d )", pontosCasca[i].x, pontosCasca[i].y);
-		}
 	}
 		
-	
-	
-	else
-	printf("Quantidade de pontos insuficiente.\n");
-	
-	
-	
-	
-	
-	
 	return 0;
 }
