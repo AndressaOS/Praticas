5951370ba8f59782022a705bf9d8f3167e9211ce
AndressaOS <andressaoliveirasouz@gmail.com>
2021-04-12 18:45:58 -0300

    a

---
+++ Lista3/Exercicio1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+ void insertion_sort(vector<int>& A) {
+	int v, j;
+	
+	for (int i=1; i< A.size(); i++){
+		v = A[i];
+		j = i - 1;
+	
+	while ((j >= 0 )and (A[j] > v )){
+		A[j + 1]= A[j];
+		j = j - 1;
+		A[j + 1] = v;
+
+	}
+	
+
+	}
+	
+	}
+
+int main(int argc, char** argv) {
+	
+	vector<int> a;
+	a.push_back(2);
+	a.push_back(4);
+	a.push_back(9);
+	a.push_back(10);
+	a.push_back(1);
+	a.push_back(3);
+	a.push_back(5);
+	a.push_back(7);
+
+	insertion_sort(a);
+	
+	for (int i=0; i< a.size(); i++){
+		cout<<"\n"<<a[i];
+	}
+	return 0;
+}
