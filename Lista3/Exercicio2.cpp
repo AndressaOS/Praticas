226da7121b2b076f97157ceefa4f7462eff91f7a
AndressaOS <andressaoliveirasouz@gmail.com>
2021-04-13 15:37:47 -0300

    a

--- Lista3/Exercicio2.cpp
+++ Lista3/Exercicio2.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include<vector>
+using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void johnson_trotter(int n){
+	vector<int> a;
+	
+	
+	
+	
+}
+
+ALGORITHM JohnsonTrotter(n)
+//Implements Johnson-Trotter algorithm for generating permutations
+//Input: A positive integer n
+//Output: A list of all permutations of {1,...,n}
+initialize the first permutation with 1?
+2
+?
+... n?
+while the last permutation has a mobile element do
+find its largest mobile element k
+swap k with the adjacent element k’s arrow points to
+reverse the direction of all the elements that are larger than k
+add the new permutation to the list
 
 int main(int argc, char** argv) {
 	return 0;
