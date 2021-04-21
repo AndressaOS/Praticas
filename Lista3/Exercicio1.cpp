2e4c253d19890091e569aea9748408bbfeab4933
AndressaOS <andressaoliveirasouz@gmail.com>
2021-04-20 22:42:43 -0300

    a

--- Lista3/Exercicio1.cpp
+++ Lista3/Exercicio1.cpp
@@ -11,21 +11,22 @@ using namespace std;
 		v = A[i];
 		j = i - 1;
 	
-	while ((j >= 0 )and (A[j] > v )){
-		A[j + 1]= A[j];
-		j = j - 1;
-		A[j + 1] = v;
+		while ((j >= 0 )and (A[j] > v )){
+			A[j + 1]= A[j];
+			j = j - 1;
+			A[j + 1] = v;
 
-	}
-	
-
-	}
+		}
 	
 	}
+	
+}
 
 int main(int argc, char** argv) {
 	
 	vector<int> a;
+	
+	/*//Teste 1
 	a.push_back(2);
 	a.push_back(4);
 	a.push_back(9);
@@ -33,7 +34,27 @@ int main(int argc, char** argv) {
 	a.push_back(1);
 	a.push_back(3);
 	a.push_back(5);
+	a.push_back(7);*/
+	
+	/*//Teste 2
+	a.push_back(12);
+	a.push_back(8);
 	a.push_back(7);
+	a.push_back(10);
+	a.push_back(4);
+	a.push_back(3);
+	a.push_back(6);
+	a.push_back(13);*/
+
+	//Teste 3
+	a.push_back(22);
+	a.push_back(8);
+	a.push_back(7);
+	a.push_back(10);
+	a.push_back(47);
+	a.push_back(34);
+	a.push_back(26);
+	a.push_back(23);
 
 	insertion_sort(a);
 	
