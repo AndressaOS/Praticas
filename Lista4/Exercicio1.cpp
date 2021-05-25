f3bfc06bb2b8297ae99882635502eb87e1aa508d
AndressaOS <andressaoliveirasouz@gmail.com>
2021-05-25 07:47:19 -0300

    Update Exercicio1.cpp

--- Lista4/Exercicio1.cpp
+++ Lista4/Exercicio1.cpp
@@ -3,40 +3,52 @@
 using namespace std;
 
 
-void merge(vector<int>& A, int p, int q, int r){
-	int i, j;
-	int B[A.size()];
-	for( i=p; i<q; i++){
-		B[i]=A[i];
-		
-	}
-	for( j=q+1; j<r; j++){
-		B[r+q+1-j]=A[j];
-		
-	}
-	i=p;
-	j=r;
+void merge(vector<int> B, vector<int> C, vector<int>& A){
+	int i=0, j=0, k=0;
 	
-	for(int k=p; k<r; k++){
-		if(B[i]<=B[j]){
-			A[k]= B[i];
+	while(i<B.size() && j<C.size()){
+		if(B[i]<=C[j]){
+			A[k]=B[i];
 			i++;
 		}else{
-			A[k]= B[j];
-			j--;
+			A[k]= C[j];
+			j++;
 		}
+		k++;
+	}
+	
+	if(i==B.size()){
+		for (int l=j;l<C.size(); l++){
+			A[k]= C[l];
+			k++;
+		}
+	}else{
+		for (int l=i;l<B.size(); l++){
+			A[k]= B[l];
+			k++;
+		}		
 	}
 	
 }
 
-void mergesort(vector<int>& A, int p, int r){
-	int q;
-	if(p<r){
-		q= (p+r)/2;
-			
-		mergesort(A, p, q);
-		mergesort(A, q+1, r);
-		merge(A, p, q, r);
+void mergesort(vector<int>& A){
+	int metade;
+
+	if(A.size()>1){
+		metade = A.size()/2;
+		vector<int> B, C;
+		
+		for(int i=0; i<metade; i++){
+			B.push_back(A[i]);
+		}
+		
+		for(int i=metade; i<A.size(); i++){
+			C.push_back(A[i]);
+		}
+					
+		mergesort(B);
+		mergesort(C);
+		merge(B, C, A);
 		
 	}
 
@@ -81,9 +93,10 @@ int main(int argc, char** argv) {
 	a.push_back(23);
 	a.push_back(2);
 	
-	mergesort(a, 0, a.size()-1);
+	mergesort(a);
 	
 	for(int i=0; i<a.size(); i++)
 		cout<<"\n"<<a[i];
+		
 	return 0;
 }
